e15219d5fd404bf116f770fecafd2f1f0458fc0a
Braulio Rojas <brauliojrojas@gmail.com>
2022-08-20 23:38:51 -0400

    fix navbar, fix table navigation

--- js/main.js
+++ js/main.js
@@ -1,80 +1,96 @@
 let currency = "usd";
 let forPage = "10";
+let page = "1";
 
-async function getCrypto() {
-    let response = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=${forPage}&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d`);
+function refresh(){
+    const removeTbody = document.querySelector("#crypto");
+    removeTbody.remove(); 
+
+    const table = document.querySelector("table");
+    const refreshTbody = document.createElement("tbody");
+        table.append(refreshTbody);
+    
+    load();
+}
+
+let next = document.getElementById("nxtBtn");
+    next.addEventListener("click" , nextPage);
+
+    function nextPage() {  
+        page++;
+        refresh();
+    };
+
+let previous = document.getElementById("pvuBtn");
+    previous.addEventListener("click" , previousPage);
+    
+    function previousPage() {     
+        page--;       
+        refresh();
+    };
+
+   // previous.setAttribute("aria-disabled","true");
+
+load();
+function load() {
+  async function getCrypto() {
+    let response = await fetch(
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=${forPage}&page=${page}&sparkline=false&price_change_percentage=1h%2C24h%2C7d`
+    );
     let data = await response.json();
     return data;
-}
+  }
 
-getCrypto().then(data =>{
+  getCrypto().then((data) => {
     let size = Object.keys(data).length;
-    for (let i = 0; i < size; i++){
-        let coin = data[i];
-        let range = document.createRange();
-        let currentPrice = coin.current_price;
-        let price1h = coin.price_change_percentage_1h_in_currency.toFixed(1);
-        let price24h = coin.price_change_percentage_24h_in_currency.toFixed(1);
-        let price7d = coin.price_change_percentage_7d_in_currency.toFixed(1);
-        const tableData = `
+    for (let i = 0; i < size; i++) {
+      let coin = data[i];
+      let range = document.createRange();
+      let currentPrice = coin.current_price;
+      let price1h = coin.price_change_percentage_1h_in_currency.toFixed(1);
+      let price24h = coin.price_change_percentage_24h_in_currency.toFixed(1);
+      let price7d = coin.price_change_percentage_7d_in_currency.toFixed(1);
+      const tableData = `
             <td>${coin.market_cap_rank}</td>
-            <td><img src="${coin.image}"></img> ${coin.name} [${coin.symbol}]</td>
-            <td>${currentPrice.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</td>
-            <td class="value">${price1h}%</td>
-            <td class="value">${price24h}%</td>
-            <td class="value">${price7d}%</td>
-            <td>${coin.total_volume.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</td>
-            <td>${coin.market_cap.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</td>          
+            <td><img src="${coin.image}"></img> ${coin.name}</td>
+            <td>${currentPrice.toLocaleString("en-US", {
+              style: "currency",
+              currency: "USD",
+            })}</td>
+            <td id="1h">${price1h}%</td>
+            <td id="24h">${price24h}%</td>
+            <td id="7d">${price7d}%</td>
+            <td>${coin.total_volume.toLocaleString("en-US", {
+              style: "currency",
+              currency: "USD",
+            })}</td>
+            <td>${coin.market_cap.toLocaleString("en-US", {
+              style: "currency",
+              currency: "USD",
+            })}</td>          
         `;
 
-        range.selectNode(document.getElementsByTagName("tr").item(i));
-        const documentFragment = range.createContextualFragment(tableData);
-        const table = document.querySelector("tbody");  
-        table.append(documentFragment);
-        /*const as = document.getElementsByClassName('value');
-        for (const a of as) {
+      range.selectNode(document.getElementsByTagName("tr").item(i));
+      const documentFragment = range.createContextualFragment(tableData);
+      const tbody = document.querySelector("tbody");
+            tbody.setAttribute("id","crypto");
+      tbody.append(documentFragment);
+      
+      const as = document.getElementById("1h");
+      //  for (const a of as) {
             if (price1h <= -0) {
-                a.style.color = 'red';
+                as.style.color = 'red';
             } else {
-                a.style.color = 'green';
+                as.style.color = 'green';
             }
-        }*/
-
+       // }
     }
-});
-/*
-function getCrypto(done) {
-    const results = fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=${forPage}&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d`);
+  });
 
-    results
-        .then(response => response.json())
-        .then(data => {
-            done(data)
-        });
+  const checkPvu = document.querySelector("#pvu");
+    if (page <= 1) {
+        checkPvu.classList.add("disabled");
+    } else {
+        checkPvu.classList.remove("disabled");
+    }
 }
-
-getCrypto(data => {
-
-    let size = Object.keys(data).length;
-    for (let i = 0; i < size; i++){
-        let coin = data[i];
-        let range = document.createRange();
-        let currentPrice = coin.current_price;
-        const tableData = `
-            <td>${coin.market_cap_rank}</td>
-            <td><img src="${coin.image}"></img> ${coin.name} [${coin.symbol}]</td>
-            <td>${currentPrice.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</td>
-            <td>${coin.price_change_percentage_1h_in_currency.toFixed(1)}%</td>
-            <td>${coin.price_change_percentage_24h_in_currency.toFixed(1)}%</td>
-            <td>${coin.price_change_percentage_7d_in_currency.toFixed(1)}%</td>
-            <td>${coin.total_volume.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</td>
-            <td>${coin.market_cap.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</td>          
-        `;
-       
-        range.selectNode(document.getElementsByTagName("tr").item(i));
-        const documentFragment = range.createContextualFragment(tableData);
-        const table = document.querySelector("tbody");  
-        table.append(documentFragment);
-    }
-   
-});*/
